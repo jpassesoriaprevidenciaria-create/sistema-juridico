3625c6d941726d117e763702c0b963110edfe761
jpassesoriaprevidenciaria-create <jpassesoriaprevidenciaria@gmail.com>
2026-02-08 07:52:42 -0300

    Update models.py

--- core/core/models.py
+++ core/core/models.py
@@ -1,1 +1,17 @@
+from django.db import models
+from django.utils import timezone
+
+class Prazo(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField(blank=True)
+    data_final = models.DateField()
+    ativo = models.BooleanField(default=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+    @property
+    def vencido(self):
+        return self.data_final < timezone.now().date()
 
