8dbed0af95bd38c16377960207998cf493512d6b
jpassesoriaprevidenciaria-create <jpassesoriaprevidenciaria@gmail.com>
2026-02-08 09:47:33 -0300

    Update admin.py

--- core/core/admin.py
+++ core/core/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import Prazo
 
 admin.site.register(Prazo)
+from django.contrib import admin
+from .models import Cliente, Processo, Prazo
+
+admin.site.register(Cliente)
+admin.site.register(Processo)
+admin.site.register(Prazo)
